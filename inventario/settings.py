079d910f293642e16e10f39fb6c56c5306133ee5
richardg20 <rich.gutierrez@duocuc.cl>
2024-05-16 22:22:15 -0400

    ds

--- inventario/settings.py
+++ inventario/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
